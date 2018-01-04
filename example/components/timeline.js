bb1874ac8a14139f796d587a7a35c482b1ef13e0
lzztouchsky <lizhanzhoubj@126.com>
2018-01-04 10:07:04 +0800

    尝试修复

--- example/components/timeline.js
+++ example/components/timeline.js
@@ -1,13 +1,35 @@
 import React from 'react';
-import Wavesurfer from '../../lib/react-wavesurfer';
-import Timeline from '../../lib/plugins/timeline';
+import Wavesurfer from '../../src/react-wavesurfer';
+import Timeline from '../../src/plugins/timeline';
 
+const dataFile = [
+  {
+    wfm:
+      'http://audio.muzhifm.com/muzhiyunzhizuo/soundFile/3f411db6-d87c-4ed0-a789-4d2fdc2e3784.mp3.wfm',
+    file:
+      'https://audio.muzhifm.com/Audios/studio-cloud/materials/4316EAAA-548C-4FEF-89DE-76C74C35D3BD.aac'
+  },
+  {
+    wfm:
+      'https://audio.muzhifm.com/muzhiyunzhizuo/soundFile/06fc181e-bcd3-4cd6-b310-37afe180cee7.mp3.wfm',
+    file:
+      'https://audio.muzhifm.com/muzhiyunzhizuo/soundFile/06fc181e-bcd3-4cd6-b310-37afe180cee7.mp3'
+  },
+  {
+    wfm:
+      'http://audio.muzhifm.com/muzhiyunzhizuo/soundFile/3f411db6-d87c-4ed0-a789-4d2fdc2e3784.mp3.wfm',
+    file:
+      'http://audio.muzhifm.com/muzhiyunzhizuo/soundFile/3f411db6-d87c-4ed0-a789-4d2fdc2e3784.mp3'
+  }
+];
 class TimelineExample extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      audioFile: '../resources/demo.wav',
+      audioFile:
+        'https://audio.muzhifm.com/muzhiyunzhizuo/soundFile/06fc181e-bcd3-4cd6-b310-37afe180cee7.mp3',
+      peacks: {},
       playing: false
     };
     this.handleTogglePlay = this.handleTogglePlay.bind(this);
@@ -19,23 +41,53 @@ class TimelineExample extends React.Component {
     });
   }
 
+  nextMusic(index) {
+    const file = dataFile[index];
+    window.fetch(file.wfm)
+      .then(response => response.arrayBuffer())
+      .then(data => {
+        this.setState({
+          peacks: new Int16Array(data, 568)
+        });
+      });
+    this.setState({ audioFile: file.file });
+  }
+
   render() {
     const timelineOptions = {
-      timeInterval: 0.5,
       height: 30,
       primaryFontColor: '#00f',
       primaryColor: '#00f'
     };
+    let peaksProp = {};
+    if (this.state.peacks.length > 0) {
+      peaksProp = {
+        audioPeaks: this.state.peacks
+      };
+    }
+    let audioFileProp = {};
+    if (this.state.audioFile) {
+      audioFileProp = {
+        audioFile: this.state.audioFile
+      };
+    }
     return (
       <div className="example col-xs-12">
         <h3 onClick={this.handleTogglePlay}>Timeline</h3>
+        {dataFile.map((f, index) => (
+          <button key={index} onClick={() => this.nextMusic(index)}>
+              切歌 {index}
+          </button>
+          ))}
         <Wavesurfer
-          audioFile={this.state.audioFile}
+          {...audioFileProp}
+          {...peaksProp}
           playing={this.state.playing}
+          options={{
+            backend: 'MediaElement'
+          }}
         >
-          <Timeline
-            options={timelineOptions}
-          />
+          <Timeline options={timelineOptions} />
         </Wavesurfer>
       </div>
     );
